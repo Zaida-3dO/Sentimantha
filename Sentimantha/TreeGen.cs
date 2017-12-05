10e5250fb24215060dcd329ffd91da7b673c9460
Phillz <phillzmike@gmail.com>
2017-12-05 21:46:37 +0100

    started tree kernel guy

--- Sentimantha/TreeGen.cs
+++ Sentimantha/TreeGen.cs
@@ -4,10 +4,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Sentimantha.Tree;
+using Sentimantha.TextAnalyzer;
 namespace Sentimantha {
-    public static class TreeGen {
-        public static TreeKernel Generate(List<string> ExpandedText) {
-            throw new NotImplementedException();
+    public static class TreeGen {        
+        /// <summary>
+        /// Generates a tree kernel from the specified expanded text.
+        /// </summary>
+        /// <param name="expandedText">The expanded text.</param>
+        /// <returns>A tree kernel based on the expanded text</returns>
+        public static TreeKernel Generate(List<string> expandedText) {
+            var tree = new TreeKernel();
+            foreach(var word in expandedText){
+                if(word.StartsWith("@")){
+
+                }                                             
+                    
+            }
         }
     }
 }
